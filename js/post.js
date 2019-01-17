e31d484b7eed993ce904ad4854c877f732d99c31
Batjaa Batbold <batjaa0615@gmail.com>
2019-01-17 15:15:06 +0100

    Test if deploy works

---
+++ js/post.js
@@ -0,0 +1,26 @@
+window.postLoad = function() {
+
+  var primaryColor = $.Color($('.nav-toggle').css('background-color'));
+
+  
+    const googleMapsSettings = {
+      styled: true,
+      latitude: 39.9777792,
+      longitude: -74.9617152,
+      zoom: 8,
+      hue: primaryColor.toRgbaString(),
+      gamma: 1.75,
+      saturation: -80,
+      lightness: -10,
+      invertLightness: false,
+      infoWindowContentString: '<h4>Info Window</h4>' + '<p>You can add content here</p>',
+    };
+
+    // Activate Google Maps
+    var mapCanvas = $('#map-canvas');
+    if (mapCanvas.length > 0){
+      mapCanvas.googleMaps(googleMapsSettings);
+    }  
+  
+
+};
